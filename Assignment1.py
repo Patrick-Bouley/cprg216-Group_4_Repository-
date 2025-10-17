5e918e5c04648c07959817a6b5f70cb1b989b382
Joshunatoir <joshunatoir2@gmail.com>
2025-10-16 22:07:48 -0600

    update to lines 5-6 and 23-24 with a formating function which fixes the issue where if someone input's a weird upper case letter in yes or no then it changes all letters to lower case to make it compatable

--- Assignment1.py
+++ Assignment1.py
@@ -2,7 +2,8 @@
 #This program stores students name and their accumulative GPA using a dictionary.
 print("Welcome to the Grade Registry Program")
 students = {} #Dictionary to store student names and GPA's.
-add_student = input("Would you like to add a new student ? y(yes),n(no)\n")
+add_student_raw = input("Would you like to add a new student ? y(yes),n(no)\n")
+add_student = (add_student_raw.lower()) # this function changes all the user inputs into lowercase for error reduction
 #Main loop to add students until the user decides to stop.
 while add_student != "no" or add_student != "n": #Check if user wants to add a student.
     if add_student == "yes" or add_student == "y":
@@ -19,7 +20,8 @@ while add_student != "no" or add_student != "n": #Check if user wants to add a s
         else:
             average = 0
         students[name] = average #Store the student's name and GPA.
-        add_student = input("Would you like to add a new student ? y(yes),n(no)\n")
+        add_student_raw = input("Would you like to add a new student ? y(yes),n(no)\n")
+        add_student = (add_student_raw.lower()) 
     #If user wants to stop.
     elif add_student == "no" or add_student == "n":
         break
