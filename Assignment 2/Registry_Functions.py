8e02eda9c57d70e9ba6b3e468b7c8411729dcf98
Patrick-Bouley <patrick.bouley@edu.sait.ca>
2025-11-26 23:45:39 -0700

    Edited Comments, Provided each function with a comment line for documentation, and added a rundown of how the program works. Added the Output of The test as a file as well

--- Assignment 2/Registry_Functions.py
+++ Assignment 2/Registry_Functions.py
@@ -3,11 +3,13 @@ students = {}
 id = []
 
 def main_menu():
+    # A function that displays the main menu and provides a choice for the user to main. It returns that choice so the overal program knows where to send the user
     print("What would you like to do today?\n-Find a student? \t\t\t\tenter 1\n-Edit a student's name using student ID? \tenter 2 \n-Add a new student? \t\t\t\tenter 3 \n-Remove a student? \t\t\t\tenter 4")
     choice = input(": ")
     return choice #sends the input back to the program file for the menu there
        
 def run_add(): #- Removed students (dictionary) argument as it has been made a global variable
+    # A function that runs the add students function, until the user decides they are done
     while True:
         add_student()
         add = input("Do you want to add a new student?y(yes)/n(no)\n").lower().strip()
@@ -17,6 +19,7 @@ def run_add(): #- Removed students (dictionary) argument as it has been made a g
             break   
     
 def run_search(): 
+    # A function that runs the search function, until the user decides they are done
     while True: # A while statement is used so that we can take input as many times as the user would like to use the program as well as to 
         id_str = input("Enter the student ID you want to search for, enter -1 to return to the main menu: \n")
         if id_str == '-1': #checking if the user wants to return to the main menu first 
@@ -28,7 +31,8 @@ def run_search():
             search(id) #runs the search function with the converted input
            
 
-def run_edit(): #a function to run the edit student function until the user decides they are done
+def run_edit(): 
+    # a function to run the edit student function until the user decides they are done
     while True:
         edit_name()
         edit = input("Do you wish to edit another students name? y(yes)/n(no)\n ").lower()
@@ -37,7 +41,8 @@ def run_edit(): #a function to run the edit student function until the user deci
         else:  
             break
 
-def run_remove(): # a function to run the remove student function to remove as many students as desired | Also nearly identical to the function above
+def run_remove(): 
+    # a function to run the remove student function to remove as many students as desired | Also nearly identical to the function above
     while True:
         remove()
         rem = input("Do you want to remove another student? y(yes)/n(no)\n")
@@ -47,10 +52,11 @@ def run_remove(): # a function to run the remove student function to remove as m
             break
     
 
-def add_student(): #Adds the student information to a list - Removed students (dictionary) argument as it has been made a global variable
-    #removed while loop as it was redundant and breaking the program by locking it into the add while loop. 
+def add_student(): 
+    #Adds the student information to a list - Removed students (dictionary) argument as it has been made a global variable
     valuelist = []
     id = int(input("Enter id of the student, followed by the student's information.\nID:\n")) #this will break if not given a int type input
+    #removed while loop as it was redundant and breaking the program by locking it into the add while loop. 
     if id in students:
         print("Student ID is already in the Registry") #prevents user from remaking a current student
         display_dict() #shows the current dictionary so that the user can see any taken id's
@@ -62,10 +68,11 @@ def add_student(): #Adds the student information to a list - Removed students (d
         students[id] = valuelist # adds a new id(key)
         print("Student added") #user confirmation in two parts, telling the user that its added and showing the user the new addition
         display_specific(id) 
-# Added a loop to check if the id is already in use
+    # Added a loop to check if the id is already in use
     
 
-def display_dict(): #for displaying all the keys and the related values within the students dictionary
+def display_dict(): 
+    #for displaying all the keys and the related values within the students dictionary
     print("The current record of students in this class are as follows:")
     for id in students:
          print("Student ID:",str(id) + ",", "Name:",f"{students[id][0]}" + ",", "GPA:",f"{students[id][1]}"+ ",", "Semester:",f"{students[id][2]}")
@@ -73,9 +80,10 @@ def display_dict(): #for displaying all the keys and the related values within t
 def display_specific(id):
     print("Student ID:",str(id) + ",", "Name:",f"{students[id][0]}" + ",", "GPA:",f"{students[id][1]}"+ ",", "Semester:",f"{students[id][2]}")
 # Made it so the display changes to be on one line, but still looks clean and you can understand it
-# ID needs to be converted to a string as you cant add an int and a string. just to make the commas work. 
+# ID needs to be converted to a string as you cant add an int and a string. just to make the commas work. using end = "," adds way too many commas and makes the text look ugly
 
-def search(id): # Searches for a student based off of their ID 
+def search(id): 
+    # Searches for a student based off of their ID 
     if id in students:
         print("Student found")
         display_specific(id) # displaying the student info consistently by using a display function
@@ -83,6 +91,7 @@ def search(id): # Searches for a student based off of their ID
         print("Student not in Directory") #error message
 
 def remove():
+    # Will remove all information on a student based off of the ID that is inputted
     id = int(input("Enter the ID of the student you want to remove from the Students' Registry:\n")).strip() #gets id from user, will break program if it isn't an integer input though
     if id in students: #checks if the id is in the students dictionary
         del students[id] #removes the student from the dictionary
@@ -91,6 +100,7 @@ def remove():
         print("Student not in directory") #error message
 
 def edit_name(): 
+    # Edits only the name of a student in the program based off the ID that is inputed
     num = int(input("Enter the id of the student. Enter -1 to return to the previous menu.\n")) #gets id from user, will break program if it isn't an integer input though
     if num in students: #checks if the id is in the students dictionary
         new_name = input("Enter the new name of the student:\n") 
--- Assignment 2/Registry_Program.py
+++ Assignment 2/Registry_Program.py
@@ -1,6 +1,37 @@
 from Registry_Functions import *
 # This imports all the functions and lets us use them as is, without the file name in front of the function name
 # import modified_test makes it so we have to write modified_list.function_name to use anything from that file. so the above way is more convienient 
+
+'''
+A rundown on how the program works
+
+As the program starts, you are presented with a menu and a couple of options. You have to input what you would like to do from those options. Search, Edit, Add, Remove. (1, 2, 3, 4) any other input will be invalid.
+As we are just starting the program, no data will be present, so you should start by adding a student. in the Terminal, type the Number '3'
+In this sub-menu it will ask for a students ID, their name, their GPA, and the semseter they are in, in that order. You will provide this information with your own inputs, in the Terminal one at a time. Each option is clearly labeled
+When a student has succesfully been added, it will display their information in the Terminal, (ID, Name, GPA, Semester) then ask if you would like to add another student. giving you the choice to say yes or no. 
+Based on this choice, it will either repeat the student adding process. or ask if you would like to continue or exit the program, giving you another yes or no option.
+Typing yes will return you to the main menu, where you will be presented with the previous options of 1, 2, 3, 4 (Search, Edit, Add, Remove)
+From here, since there is data in the program now. We can select any of the other options to use. in the terminal Lets type the number '2'. Which is Edit
+In the edit section, you will only be allowed to edit a students name that is in the program. 
+It will first ask you to input the ID of the student you wish to edit. which you can then do so in the Terminal. Once the ID has been inputed, it will ask you to provide the new/edited name of the student, which you can provide in the terminal as well
+After getting that info, it will the provide you with a confirmation that a student with the ID you inputed, now has the new name you provided.
+After that, you will again be presented the option to do the same process again. If you dont, it will ask if you wish to continue, or end the program again. Each of the choices in the main menu will repeat this process.
+Once back at the main menu, you are provided with the same options 1, 2, 3, 4. Lets type the number '1' which is Search
+The search sub menu will open. and this one is pretty simple. You will be asked to search for the students information you want by using their ID. Input that in the Terminal. You will be provided that information in the same way you added a student. Their ID, Name, GPA, and Semseter.
+It will then repeat its statement of "Enter the id of the student you wish to find, or press -1 to exit" pressing -1 will prompt you with another "continue? yes or no" input 
+Once back at the main menu, we see our options. 1, 2, 3, 4. The last option being '4', we will select that one.
+The last function is to remove the entirety of a students information from the program. 
+The sub menu will open and ask you for the students ID you wish to delete. which you can do so in the Terminal. once that ID has been submitted, it will delete that student and all their information.
+The consol will state "Student Deleted" meaning it was successful, and repeat its process and ask if you want to delete another student. pressing -1 will stop this process. giving you one last "do you want to continue" option.
+If you select no, the menu will close and exit the program
+
+Its important to note that this program does not save any of the information that gets inputed. It is all lost once you exit
+Statements that ask you for IDs will also have failsafes for inputing information that is not correct or already in use
+If trying to add a student with an ID already in the system, the terminal will state that the ID is already in use and will return you to a point where you can try inputting the ID again 
+If using the search, edit or remove functions, It will let you know if that ID doesnt exist in the system and return to where you can try inputing the ID again
+
+'''
+
 print("Welcome to the students record program!")
 test = True 
 while test == True:
