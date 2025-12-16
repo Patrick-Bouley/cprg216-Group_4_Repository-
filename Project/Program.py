f2f24aacf6880890bf155677709fb4655bb7f490
Patrick-Bouley <patrick.bouley@edu.sait.ca>
2025-12-15 18:31:44 -0700

    Added Comments at top of program

--- Project/Program.py
+++ Project/Program.py
@@ -1,5 +1,16 @@
 from Car import *
 
+'''
+Created By Patrick Bouley, Joshua Dyke, Ahmed Yassine Messaoudi. Group 4
+December, 15, 2025
+
+This program was made to register, and save a cars information to a database. Using a class system to gather information.  
+The user can come back to the same database whenever they need to add, edit or remove cars.
+They can view all the information saved on the file as well. 
+A file will automatically be created if one doesnt exist. 
+
+'''
+
 print("Welcome to the cars inventory system.")
 load() # this is defined in the import page, meaning the user needs to have a file called "Saved_data.txt" ready to use before running this program.
 operate = True 
